7ffaacf1387978bb25bc132403ccf194dee5f231
gballege <gilles.ballegeer@ugent.be>
2019-01-13 12:12:50 +0100

    Started UI for merge

--- pdf_merge.py
+++ pdf_merge.py
@@ -15,6 +15,10 @@ class PDFMerge(TabBaseClass):
         self.pdf_files = []
         self.move_button_size = QSize(25, 25)
 
+        self.frame_list = []
+        self.up_button_list = []
+        self.down_button_list = []
+
     def confirmUI(self, widgets):
         self.widgets = widgets
         self.widgets.MERGE_AddPDFButton.clicked.connect(self.load_new_pdf)
@@ -30,12 +34,20 @@ class PDFMerge(TabBaseClass):
             self.pdf_files.append(file)
 
             up_down_layout = QVBoxLayout()
-            up_button = QPushButton("\u2304")
+            up_button = QPushButton("\u2303")
             up_button.setFixedSize(self.move_button_size)
-            down_button = QPushButton("\u2303")
+            down_button = QPushButton("\u2304")
             down_button.setFixedSize(self.move_button_size)
-            up_down_layout.addWidget(down_button)
+
+            up_button.clicked.connect(self.move_up)
+            down_button.clicked.connect(self.move_down)
+
+            self.up_button_list.append(up_button)
+            self.down_button_list.append(down_button)
+
             up_down_layout.addWidget(up_button)
+            up_down_layout.addWidget(down_button)
+
             up_down_layout.setSpacing(1)
 
             pdf_name = os.path.split(file)[1]
@@ -52,12 +64,43 @@ class PDFMerge(TabBaseClass):
             new_frame.setContentsMargins(0, 0, 0, 0)
             new_frame.setFixedSize(QSize(width,height))
             new_frame.setLayout(main_layout)
-
             new_frame.setFrameStyle(QFrame.WinPanel | QFrame.Plain)
+
+            self.frame_list.append(new_frame)
+
             main_layout.addLayout(up_down_layout)
             main_layout.addWidget(label)
 
-            self.widgets.MERGE_PDFsVLayout.addWidget(new_frame)
+            # because of spacer at the bottom
+            self.widgets.MERGE_PDFsVLayout.insertWidget(len(self.up_button_list) - 1, new_frame)
+
+    def move_up(self):
+        sender = self.parent.sender()
+        index = self.up_button_list.index(sender)
+        if index != 0:
+            self.swap(index, index - 1)
+
+    def move_down(self):
+        sender = self.parent.sender()
+        index = self.down_button_list.index(sender)
+        if index != len(self.down_button_list) - 1:
+            self.swap(index, index + 1)
+
+    def swap(self, index1, index2):
+        # make index2 bigger then index1
+        if index2 < index1:
+            index1, index2 = index2, index1
+
+        self.up_button_list[index1], self.up_button_list[index2] = self.up_button_list[index2], self.up_button_list[index1]
+        self.down_button_list[index1], self.down_button_list[index2] = self.down_button_list[index2], self.down_button_list[index1]
+
+        frame2 = self.widgets.MERGE_PDFsVLayout.takeAt(index2)
+        frame1 = self.widgets.MERGE_PDFsVLayout.takeAt(index1)
+
+        self.pdf_files[index1], self.pdf_files[index2] = self.pdf_files[index2], self.pdf_files[index1]
+
+        self.widgets.MERGE_PDFsVLayout.insertWidget(index1, frame2.widget())
+        self.widgets.MERGE_PDFsVLayout.insertWidget(index2, frame1.widget())
 
     def handle_resizing(self):
         pass
